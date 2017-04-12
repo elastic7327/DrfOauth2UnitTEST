3a8d332bd3b040a48b95ed42b087810a2a357085
elastic <elastic7327@gmail.com>
2017-04-12 17:30:17 +0900

    Oauth2 TDD

---
+++ src/lists/tests/base.py
@@ -0,0 +1,48 @@
+from datetime import timedelta
+from django.urls import reverse
+
+from django.utils import timezone
+from django.test import TestCase
+from django.contrib.auth.models import User, Group
+
+from oauth2_provider.models import get_application_model, AccessToken
+from oauth2_provider.settings import oauth2_settings
+from oauth2_provider.tests.test_utils import TestCaseUtils
+
+from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse
+
+Application = get_application_model()
+
+class BaseTest(APITestCase, TestCaseUtils):
+
+    """Test case docstring."""
+
+    def setUp(self):
+        self.norm_user =  User.objects.create(username='12345', password='12345')
+        self.admin_user = User.objects.create(
+                                    username="Daniel",
+                                    password="as", 
+                                    is_superuser=True,
+                                    is_staff=True)
+        self.application = Application(
+                name='lists',
+                user=self.admin_user,
+                client_type=Application.CLIENT_PUBLIC,
+                authorization_grant_type=Application.GRANT_PASSWORD
+                )
+        self.application.save()
+
+    def tearDown(self):
+        pass
+
+    def get_token(self, username, password, application, client):
+         access_user = User.objects.get(username=username)
+         access_token = AccessToken.objects.create(
+             user=access_user,
+             scope='read write',
+             expires=timezone.now() + timedelta(seconds=300),
+             token='secret-access-token-key{0}'.format(username),
+             application=application
+         )
+         return access_token.token
---
+++ src/lists/tests/test_views.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from django.urls import reverse
+
+from django.utils import timezone
+from django.contrib.auth.models import User, Group
+
+from .base import BaseTest
+
+class TestSmoke(BaseTest):
+
+    def test_created_user_check(self):
+        self.assertEqual(User.objects.count(), 2)
+
+    def test_retrive_without_authentication_token(self):
+        response = self.client.get('/api/users')
+        self.assertEqual(response.status_code, 401)
+
+    def test_create_toke_and_get_some_authentication(self):
+        norm_users_token = self.get_token(self.norm_user.username,
+                                            self.norm_user.password,
+                                            self.application,
+                                            self.client
+        )
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer {0}'.format(norm_users_token))
+        response = self.client.get('/api/users')
+        self.assertEqual(response.status_code, 200)
